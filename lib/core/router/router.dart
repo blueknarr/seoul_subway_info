3fa16031bee13f680b9ac6e8e37503650b0370c6
blueknarr <knarrblue@gmail.com>
2023-07-24 13:34:38 +0900

    Feat: go router

---
+++ lib/core/router/router.dart
@@ -0,0 +1,27 @@
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:seoul_subway_info/data/repository/subway_repository_impl.dart';
+import 'package:seoul_subway_info/presentation/main/main_view_model.dart';
+
+import '../../domain/use_case/find_nearest_station_use_case.dart';
+import '../../presentation/main/main_screen.dart';
+
+// GoRouter configuration
+final router = GoRouter(
+  initialLocation: '/main',
+  routes: [
+    GoRoute(
+      path: '/main',
+      builder: (context, state) {
+        return ChangeNotifierProvider(
+          create: (_) => MainViewModel(
+            FindNearestStationUseCase(
+              SubwayRepositoryImpl(),
+            ),
+          ),
+          child: const MainScreen(),
+        );
+      },
+    ),
+  ],
+);
